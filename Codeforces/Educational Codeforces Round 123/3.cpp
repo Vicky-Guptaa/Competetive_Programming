b738b7d8a09bc126d3cdda620f37085bb6d99e86
Vicky Gupta (PC) <vickyguptaa7@gmail.com>
2022-02-23 22:25:26 +0530

    Solved 123 Codeforcess Problem 3

--- Codeforces/Educational Codeforces Round 123/3.cpp
+++ Codeforces/Educational Codeforces Round 123/3.cpp
@@ -32,31 +32,39 @@ int main()
         int n, k;
         cin >> n >> k;
         int arr[n];
+        int presum[n + 1] = {0};
         for (int i = 0; i < n; i++)
         {
             cin >> arr[i];
-            arr[i] += k;
         }
+        for (int i = 1; i <= n; i++)
+        {
+            presum[i] = presum[i - 1] + arr[i - 1];
+        }
+        // for (auto x : presum)
+        //     cout << x << " ";
+
         vector<int> result;
-        priority_queue<pp, vector<pp>, greater<pp>> pque;
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i <= n; i++)
         {
-            pque.push({arr[i], i});
+            int max_sum = INT_MIN;
+            for (int j = 0; j <= n - i; j++)
+            {
+                max_sum = max(max_sum, presum[j + i] - presum[j]);
+            }
+            result.push_back(max_sum);
         }
-        int Max_sum = max_sum(arr, n);
-        result.push_back(Max_sum);
-        while (!pque.empty())
+        // for (auto x : result)
+        //     cout << x << " ";
+        // cout << "\n";
+        for (int i = 0; i <= n; i++)
         {
-            int ind = pque.top().second;
-            arr[ind] -= k;
-            pque.pop();
-            Max_sum = max_sum(arr, n);
-            result.push_back(Max_sum);
-        }
-        reverse(result.begin(), result.end());
-        for (auto x : result)
-        {
-            cout << x << " ";
+            int max_sum = INT_MIN;
+            for (int j = 0; j < result.size(); j++)
+            {
+                max_sum = max(max_sum, result[j] + min(i, j) * k);
+            }
+            cout << max_sum << " ";
         }
         cout << "\n";
     }
