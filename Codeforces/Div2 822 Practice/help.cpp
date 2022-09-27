97b17e8c200fb852585a6e6ee4fe131f74e4c3ee
Vicky Gupta <vickyguptaa7@gmail.com>
2022-09-27 20:34:04 +0530

    prac probs

---
+++ Codeforces/Div2 822 Practice/help.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+typedef long long ll;
+string pushDominoes(string dominoes)
+{
+    vector<int> left, right;
+    int n = dominoes.size(), indx = -1;
+    char ch = '.';
+    for (int i = 0; i < n; i++)
+    {
+        left.push_back(indx);
+        if (dominoes[i] != '.')
+            ch = dominoes[i], indx = i;
+    }
+    ch = '.';
+    indx = -1;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        right.push_back(indx);
+        if (dominoes[i] != '.')
+            ch = dominoes[i], indx = i;
+    }
+    reverse(right.begin(), right.end());
+    string result;
+    for (int i = 0; i < n; i++)
+    {
+        if (dominoes[i] != '.')
+        {
+            result += dominoes[i];
+            continue;
+        }
+        if (left[i] != -1 && dominoes[left[i]] == 'R')
+        {
+            if (right[i] != -1 && dominoes[right[i]] == 'L')
+            {
+                if (i - left[i] == right[i] - i)
+                    result += '.';
+                else if (i - left[i] > right[i] - i)
+                    result += 'R';
+                else
+                    result += 'L';
+            }
+            else
+            {
+                result += 'R';
+            }
+        }
+        else
+        {
+            if (right[i] != -1 && dominoes[left[i]] == 'L')
+            {
+                result += 'L';
+            }
+            else
+            {
+                result += '.';
+            }
+        }
+    }
+    return result;
+}
+int main()
+{
+    string str = "RR..L";
+    cout << pushDominoes(str) << endl;
+    return 0;
+}
