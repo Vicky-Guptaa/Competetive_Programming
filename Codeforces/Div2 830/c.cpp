29ad43aaf8bf0c277f405528389c3e5c3dbde057
Vicky Gupta <vickyguptaa7@gmail.com>
2022-10-24 09:52:48 +0530

    solved for practice

--- Codeforces/Div2 830/c.cpp
+++ Codeforces/Div2 830/c.cpp
@@ -199,109 +199,23 @@ bool isPerfectSquare(ll x)
 //__builtin_clz(x); for int
 //__builtin_clzll(x); for long long
 
-struct node
-{
-    ll val;
-    ll cnt;
-    // can add more if required or remove
-    node(ll v = 0, ll c = 0)
-    {
-        val = v;
-        cnt = c;
-    }
-};
-
-class segTree
-{
-public:
-    vector<node> segArr;
-    vector<ll> narr;
-
-    segTree(vector<ll> arr)
-    {
-        narr = arr;
-        int arrSize = arr.size();
-        narr = arr;
-        /*
- we are constructing tree in form of array linearly therefore
-we have to make 4*n size array for n size given array
-*/
-        segArr.resize(4 * arrSize);
-        constructSegTree(0, 0, arrSize - 1);
-    }
-
-    // here you can change the combine logic according to questions
-    node Combine(node &left, node &right)
-    {
-        return node(left.val + right.val, 0);
-    }
-
-    void constructSegTree(int index, int startIndx, int endIndx)
-    {
-        if (startIndx == endIndx)
-        {
-            // Leaf Logic
-            segArr[index] = node(narr[startIndx], 0);
-            return;
-        }
-        int midIndx = startIndx + (endIndx - startIndx) / 2;
-
-        constructSegTree(2 * index + 1, startIndx, midIndx);
-        constructSegTree(2 * index + 2, midIndx + 1, endIndx);
-        segArr[index] = Combine(segArr[2 * index + 1], segArr[2 * index + 2]);
-    }
-    node getQuery(int index, int startIndx, int endIndx, int lquery, int rquery)
-    {
-        // no overlap
-        if (startIndx > rquery || lquery > endIndx)
-            return node(0, 0);
-
-        // complete overlap
-        if (startIndx >= lquery && endIndx <= rquery)
-            return segArr[index];
-
-        // partial overlap
-        int midIndx = startIndx + (endIndx - startIndx) / 2;
-        node left = getQuery(index * 2 + 1, startIndx, midIndx, lquery, rquery);
-        node right = getQuery(index * 2 + 2, midIndx + 1, endIndx, lquery, rquery);
-
-        return Combine(left, right);
-    }
-
-    void updateQuery(int index, int startIndx, int endIndx, int position, int val)
-    {
-        if (position < startIndx || position > endIndx)
-            return;
-
-        if (startIndx == endIndx)
-        {
-            // Leaf Condition
-            segArr[index] = node(val, 0);
-            narr[startIndx] = val;
-            return;
-        }
-
-        int midIndx = startIndx + (endIndx - startIndx) / 2;
-        updateQuery(2 * index + 1, startIndx, midIndx, position, val);
-        updateQuery(2 * index + 2, midIndx + 1, endIndx, position, val);
-        segArr[index] = Combine(segArr[2 * index + 1], segArr[2 * index + 2]);
-    }
-
-    // Use To Get The result of the queries
-    void updateQuery(int postion, int value)
-    {
-        updateQuery(0, 0, (int)narr.size() - 1, postion, value);
-    }
-
-    node getQuery(int lquery, int rquery)
-    {
-        return getQuery(0, 0, (int)narr.size() - 1, lquery, rquery);
-    }
-};
-// use 0 based indexing while calling the update and query func 
-
-
 // Code
+
+ll calc(vll &preSum, vll &preXor, ll size, ll ans, ll &l, ll &r)
+{
+    ll n = preSum.size();
+    fl(i, size, n)
+    {
+        if (ans == preSum[i] - preSum[i - size] - (preXor[i] ^ preXor[i - size]))
+        {
+            l = i - size + 1;
+            r = i;
+            return true;
+        }
+    }
+    return false;
+}
+
 void solve()
 {
     ll n, q;
@@ -310,8 +224,31 @@ void solve()
     cin >> arr;
     ll l, r;
     cin >> l >> r;
-    ll ans = 0, res = 0, preX = 0, preS = 0;
-
+    ll low = 1, high = n;
+    vll preSum(n + 1, 0), preXor(n + 1, 0);
+    fl(i, 1, n + 1)
+    {
+        preSum[i] = preSum[i - 1] + arr[i - 1];
+        preXor[i] = preXor[i - 1] ^ arr[i - 1];
+    }
+    ll ans = preSum[n] - preXor[n], left = 1, right = n;
+    while (low <= high)
+    {
+        ll mid = (low + high) / 2;
+        ll l, r;
+        ll ans1 = calc(preSum, preXor, mid, ans, l, r);
+        if (ans1)
+        {
+            high = mid - 1;
+            left = l;
+            right = r;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    cout << left << " " << right << "\n";
 }
 /*
 When you are coding,remember to:
