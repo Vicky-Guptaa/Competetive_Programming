56828d30c183ed4d6c368999a7a0923eb2e0ae0b
Vicky Gupta <vickyguptaa7@gmail.com>
2022-10-29 09:58:45 +0530

    solved cf probs for practice

--- Cp Prac/Rating Wise/1600/Help.cpp
+++ Cp Prac/Rating Wise/1600/Help.cpp
@@ -1,64 +1,41 @@
 #include <iostream>
+#include <math.h>
 #include <vector>
-#include <queue>
+#include <algorithm>
+#include <map>
 using namespace std;
 
-class Graph
+typedef long long ll;
+vector<int> findErrorNums(vector<int> &arr)
 {
-    int V;
-    vector<vector<int>> adj;
-public:
-    Graph(int V)
+    vector<int> res;
+    for (int i = 0; i < arr.size(); i++)
     {
-        this->V = V;
-        adj.resize(V);
-    }
-    void addEdge(int u, int v)
-    {
-        adj[u].push_back(v);
-    }
-    vector<int> bfsGraph(int s)
-    {
-        vector<int> bfs;
-        int visited[V] = {0};
-        queue<int> q;
-        visited[0] = 1;
-        q.push(0);
-        while (!q.empty())
+        if (arr[abs(arr[i]) - 1] < 0)
         {
-            int node = q.front();
-            q.pop();
-            bfs.push_back(node);
-            for (auto it : adj[node])
-            {
-                if (!visited[it])
-                {
-                    visited[it] = 1;
-                    q.push(it);
-                }
-            }
+            res.push_back(abs(arr[i]));
         }
-        return bfs;
+        else
+        {
+            arr[arr[i] - 1] = -arr[arr[i] - 1];
+        }
     }
-};
-
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] > 0)
+        {
+            res.push_back(i + 1);
+            break;
+        }
+    }
+    return res;
+}
 int main()
 {
-    Graph g(4);
-    g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(1, 2);
-    g.addEdge(2, 0);
-    g.addEdge(2, 3);
-    g.addEdge(3, 3);
-
-    cout << "Following is Breadth First Traversal "
-         << "(starting from vertex 2) \n";
-    vector<int> bfs = g.bfsGraph(2);
-    for (auto x : bfs)
-    {
+    vector<int> arr = {2, 3, 2}, brr = {2, 14, 10};
+    brr = findErrorNums(arr);
+    for (auto x : brr)
         cout << x << " ";
-    }
 
     return 0;
 }
