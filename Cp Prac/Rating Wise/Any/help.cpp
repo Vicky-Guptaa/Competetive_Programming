54d3483aa3be7532572ffa69fe66f6d70f9a58ae
vickygupta <vickyguptaa7@gmail.com>
2023-04-14 19:20:20 +0530

    solved xenia and bit operations solved

--- Cp Prac/Rating Wise/Any/help.cpp
+++ Cp Prac/Rating Wise/Any/help.cpp
@@ -1,12 +1,34 @@
-#include<iostream>
-#include<math.h>
-#include<vector>
-#include<algorithm>
-using namespace std;
+void createMapping(int in[], map<int, int> &nodeToIndex, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        nodeToIndex[in[i]] = i;
+    }
+}
 
-typedef long long ll;
+Node *solve(int in[], int pre[], int &index, int inorderStart, int inorderEnd, int n, map<int, int> &nodeToIndex)
+{
+    if (index >= n || inorderStart > inorderEnd)
+    {
+        return NULL;
+    }
 
-int main(){
-    
-    return 0;
+    int element = pre[index++];
+    Node *root = new Node(element);
+    int position = nodeToIndex[element];
+
+    root->left = solve(in, pre, index, inorderStart, position - 1, n, nodeToIndex);
+    root->right = solve(in, pre, index, position + 1, inorderEnd, n, nodeToIndex);
+
+    return root;
+}
+Node *buildTree(int in[], int pre[], int n)
+{
+    map<int, int> nodeToIndex;
+    createMapping(in, nodeToIndex, n);
+    int preOrderIndex = 0;
+
+    Node *root = solve(in, pre, preOrderIndex, 0, n - 1, n, nodeToIndex);
+    return root;
+        
 }
