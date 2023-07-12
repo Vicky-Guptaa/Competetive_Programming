006b1c3bd5ff0b81ad9560d172ac6ba49c2212eb
vickygupta <vickyguptaa7@gmail.com>
2023-07-12 23:16:02 +0530

    solved cf probs for prac

--- Cp Prac/Rating Wise/1600/Help.cpp
+++ Cp Prac/Rating Wise/1600/Help.cpp
@@ -3,39 +3,87 @@
 #include <vector>
 #include <algorithm>
 #include <map>
+#include <queue>
 using namespace std;
 
 typedef long long ll;
-vector<int> findErrorNums(vector<int> &arr)
+vector<int> moveX = {1, -1, 0, 0};
+vector<int> moveY = {0, 0, -1, 1};
+
+int helper(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &visit)
 {
-    vector<int> res;
-    for (int i = 0; i < arr.size(); i++)
+    if (grid[i][j])
+        return false;
+    int row = grid.size(), col = grid[0].size();
+    queue<pair<int, int>> que;
+    visit[i][j] = true;
+    que.push({i, j});
+    while (!que.empty())
     {
-        if (arr[abs(arr[i]) - 1] < 0)
+        auto [x, y] = que.front();
+        if (x + 1 == row)
+            return true;
+        que.pop();
+        for (int i = 0; i < 4; i++)
         {
-            res.push_back(abs(arr[i]));
+            int _x = x + moveX[i], _y = y + moveY[i];
+            if (_x >= row || _y >= col || _x < 0 || _y < 0 || visit[_x][_y] || grid[_x][_y])
+                continue;
+            que.push({_x, _y});
+            visit[_x][_y] = true;
+        }
+    }
+    return false;
+}
+
+int isValid(int mid, vector<vector<int>> &cells, vector<vector<int>> &grid, vector<vector<int>> &visit)
+{
+    int row = grid.size(), col = grid[0].size();
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            grid[i][j] = 0;
+            visit[i][j] = 0;
+        }
+    }
+    for (int i = 0; i < mid; i++)
+    {
+        grid[cells[i][0] - 1][cells[i][1] - 1] = 1;
+    }
+    for (int i = 0; i < col; i++)
+    {
+        if (helper(0, i, grid, visit))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int latestDayToCross(int row, int col, vector<vector<int>> &cells)
+{
+    int low = 1, high = row * col, result = 2;
+    vector<vector<int>> grid(row, vector<int>(col, 0));
+    vector<vector<int>> visit(row, vector<int>(col, 0));
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (isValid(mid, cells, grid, visit))
+        {
+            low = mid + 1;
+            result = mid;
         }
         else
         {
-            arr[arr[i] - 1] = -arr[arr[i] - 1];
+            high = mid - 1;
         }
     }
-    for (int i = 0; i < arr.size(); i++)
-    {
-        if (arr[i] > 0)
-        {
-            res.push_back(i + 1);
-            break;
-        }
-    }
-    return res;
+    return result;
 }
 int main()
 {
-    vector<int> arr = {2, 3, 2}, brr = {2, 14, 10};
-    brr = findErrorNums(arr);
-    for (auto x : brr)
-        cout << x << " ";
-
+    vector<vector<int>> cells = {{1, 1}, {2, 1}, {1, 2}, {2, 2}};
+    cout << latestDayToCross(2, 2, cells) << '\n';
     return 0;
 }
